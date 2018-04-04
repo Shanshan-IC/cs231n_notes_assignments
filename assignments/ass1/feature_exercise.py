88139d30b51927b792cb6681a29f25315d4d514c
fushanshan <fushanshan@qiyi.com>
2018-04-04 13:35:03 +0800

    note

---
+++ assignments/ass1/feature_exercise.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/3/5 17:29
+# @Author  : Shanshan Fu
+# @File    : feature_exercise.py  :
+# @Contact : 33sharewithu@gmail.com
+
+
+
+
